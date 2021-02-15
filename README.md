707acc5e9298fc054649b33f5b2ae712fa4f20a6
Peter Hawthorne <hawt0010@umn.edu>
2021-02-15 08:21:40 -0600

    Update with src, scripts and path updating

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # project_helper
 
-A simple module to help manage paths in a research project. Inspired by DrWatson.jl, but currently MUCH simpler. 
+A simple module to help manage paths in a research project. Inspired by [DrWatson.jl](https://github.com/JuliaDynamics/DrWatson.jl), but much simpler. 
 
-Assumes that you want data, results, and workspace folders that can be reached by, e.g., 
-`project.data_folder()`, which will return a `pathlib.Path` object pointing to the 
-appropriate folder.
+Assumes that you want data, results, scripts, src, and workspace folders that can be reached by, e.g., `project.data_folder`, which will return a `pathlib.Path` object pointing to the appropriate folder.
 
 This is particularly intended to help with project portability - as long as all project data
 and results are contained in the project folder, no absolute paths will be required, and no fussing about relative paths (e.g. number of ".."s needed to get from script to data) is required either.
@@ -13,11 +11,15 @@ and results are contained in the project folder, no absolute paths will be requi
 The way I'm using this is to have my project structured like this:
 ```
 project_dir/
-    ...
-    - src/
-        - scripts/
-            project.py
-            script.py
+    - data
+    - results
+    - scripts/      # specific runs, analyses, etc...
+        project.py
+        script.py   # imports project from project.py
+    - src/          # reused code and packages go in here
+        - module_a
+        - module_b
+    - workspace
     ...
 ```
  where `project.py` looks like:
@@ -37,5 +39,7 @@ project.init_project()
 
 save_results(project.results_folder() / "results.csv")
 ```
+
+The function **`project.init_project()`** creates folders `data`, `results`, `scripts`, `src`, and `workspace` if they are missing, and adds `src` to `sys.path` so that the modules in `src` can be imported from the various scripts. 
 
 Of course, other scripts can also import `project` from `project.py`, so all the scripts have easy access to project folders through the same `Project` object.
--- project_helper/project.py
+++ project_helper/project.py
@@ -13,8 +13,11 @@ class Project(object):
     for now callers need to put Path.cwd() in calling_path. 
     """
     def __init__(self, calling_path, project_root_name, to_skip=0,
-                 results_folder_name="results", data_folder_name="data",
-                 workspace_folder_name="workspace"):
+                 results_folder_name="results",
+                 data_folder_name="data",
+                 workspace_folder_name="workspace",
+                 src_folder_name="src",
+                 scripts_folder_name="scripts"):
         """
         Searches recursively up from calling location to find a folder named
         `project_root_name`. Will skip `skip` number of such folders. 
@@ -27,7 +30,9 @@ class Project(object):
         self.results_folder_name = results_folder_name
         self.data_folder_name = data_folder_name
         self.workspace_folder_name = workspace_folder_name
-        
+        self.src_folder_name = src_folder_name
+        self.scripts_folder_name = scripts_folder_name
+                
         p = calling_path.resolve()
         l = list(p.parts)
         skipped = 0
@@ -38,24 +43,24 @@ class Project(object):
         
         l.append(project_root_name)
         self._project_root = Path(*l)
-
-    def project_root(self):
-        return self._project_root
-    
-    def results_folder(self):
-        return self._project_root / self.results_folder_name
-    
-    def workspace_folder(self):
-        return self._project_root / self.workspace_folder_name
-    
-    def data_folder(self):
-        return self._project_root / self.data_folder_name
+        
+        self.project_root = self._project_root
+        self.results_folder = self._project_root.joinpath(self.results_folder_name)
+        self.data_folder = self._project_root.joinpath(self.data_folder_name)
+        self.workspace_folder = self._project_root.joinpath(self.workspace_folder_name)
+        self.src_folder = self._project_root.joinpath(self.src_folder_name)
+        self.scripts_folder = self._project_root.joinpath(self.scripts_folder_name)
+        
     
     def init_project(self):
-        folders = [self.results_folder(), self.workspace_folder(), self.data_folder()]
+        folders = [self.results_folder, self.workspace_folder, self.data_folder,
+                   self.src_folder, self.scripts_folder]
         for folder in folders:
             if not folder.exists():
                 folder.mkdir()
+        
+        if str(self.src_folder) not in sys.path:
+            sys.path.append(str(self.src_folder))
     
     
         
